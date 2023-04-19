9c2ab7ea8bc2432b5ca572ab557d75bdd824019a
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2023-04-19 19:48:17 +0200

    content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-import './App.css';
 import Login from"../src/pages/login/login"
 import {BrowserRouter  as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/home"
 function App() {
   return (
-    <div className="App">
+    <>
       	 <Router>
 			        <Routes>
                 <Route path ="/" element={<Home/>}/>
                 <Route path ="/Login" element={<Login/>}/>  
             </Routes>
          </Router>
-    
-    </div>
+    </>
   );
 }
 
